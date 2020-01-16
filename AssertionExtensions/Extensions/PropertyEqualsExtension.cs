72af622d6eb14ed072e904aa3d951517c0b57168
Konsta Mäenpänen <konsta.maenpanen@outlook.com>
2020-01-16 13:02:38 +0200

    Added property extension methods.

---
+++ AssertionExtensions/Extensions/PropertyEqualsExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssertionExtensions.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AssertionExtensions.Extensions
+{
+    public static class PropertyEqualsExtension
+    {
+        public static void PropertyEquals<T>(this Assert a, T expected, Func<T> propertyExpression) => 
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression);
+
+        public static void PropertyEquals<T>(this Assert a, T expected, Func<T> propertyExpression, int timeout) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, timeout: timeout);
+
+        public static void PropertyEquals<T>(this Assert a, T expected, Func<T> propertyExpression, string message) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, message: message);
+
+        public static void PropertyEquals<T>(this Assert a, T expected, Func<T> propertyExpression, string message, params object[] args) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, message: message, parameters: args);
+
+        public static void PropertyEquals<T>(this Assert a, T expected, Func<T> propertyExpression, int timeout, string message) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, timeout: timeout, message: message);
+
+        public static void PropertyEquals<T>(this Assert a, T expected, Func<T> propertyExpression, int timeout, string message, params object[] args) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, timeout: timeout, message: message, parameters: args);
+
+    }
+}
---
+++ AssertionExtensions/Extensions/PropertyNotEqualsExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssertionExtensions.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AssertionExtensions.Extensions
+{
+    public static class PropertyNotEqualsExtension
+    {
+        public static void PropertyNotEquals<T>(this Assert a, T expected, Func<T> propertyExpression) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, false);
+
+        public static void PropertyNotEquals<T>(this Assert a, T expected, Func<T> propertyExpression, int timeout) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, false, timeout);
+
+        public static void PropertyNotEquals<T>(this Assert a, T expected, Func<T> propertyExpression, string message) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, false, message: message);
+
+        public static void PropertyNotEquals<T>(this Assert a, T expected, Func<T> propertyExpression, string message, params object[] args) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, false, message: message, parameters: args);
+
+        public static void PropertyNotEquals<T>(this Assert a, T expected, Func<T> propertyExpression, int timeout, string message) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, false, timeout, message);
+
+        public static void PropertyNotEquals<T>(this Assert a, T expected, Func<T> propertyExpression, int timeout, string message, params object[] args) =>
+            PropertyAssertCore.CheckPropertyEquality(expected, propertyExpression, false, timeout, message, args);
+
+    }
+}
